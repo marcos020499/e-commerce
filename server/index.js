447f1389d9cfefe90476f0eb15e5e7cce3726bc2
validName <>
2021-04-07 10:50:34 -0500

    h

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
 app.use('/api/product', require('./routes/product'));
 
 
